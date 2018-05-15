4f8d0c96703a62586e1e602f05e6df3cb8498fea
alexsegen <alejandrojv17@gmail.com>
2018-05-15 12:57:12 -0400

    Metas

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'mynuxt',
+    title: 'HeyU App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Aplicación de mensajería instantánea' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
